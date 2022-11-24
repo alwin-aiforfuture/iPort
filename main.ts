39c3e75d44d0172aa566c5878cc3ba436964cab5
alwin-aiforfuture <118883624+alwin-aiforfuture@users.noreply.github.com>
2022-11-24 10:13:50 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
 namespace iPort {
+    /**
+     * iPort digitalWrite
+     */
+    //% blockId=Digital write
+    //% block="digitalWrite"
+    //% address.min=0 address.max=20
+    //% pin.min=0 pin.max=100
     export function digitalWrite(address: number, pin: number): number {
         return 10 * pin
     }
