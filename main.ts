902d6c3828e65e427ca8617b44b763bfaefc97d1
alwin-aiforfuture <118883624+alwin-aiforfuture@users.noreply.github.com>
2022-11-24 11:38:43 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -180,7 +180,7 @@ namespace iPort {
      * iPort 7-seg dispaly set brightness
      */
     //% blockId=sevenSegment_SetBrightness
-    //% block="iPort #$address set brightness $num"
+    //% block="iPort #$address set brightness $brightness"
     //% address.min=0 address.max=20 address.defl=10
     //% brightness.min=0 brightness.max=7
     //% group="7-seg dispaly" blockGap=10
