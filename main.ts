de32f8482716462fd70ef9e346c25c75af9d38be
alwin-aiforfuture <118883624+alwin-aiforfuture@users.noreply.github.com>
2023-02-03 15:55:05 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -700,6 +700,7 @@ namespace iPort {
         let i2c_buf = i2c_receive_n_byte(address, checksum, "0x81", 4)
         let value = i2c_buf[0] << 24 | i2c_buf[1] << 16 | i2c_buf[2] << 8 | i2c_buf[3]
 
-        return hex_to_float(value)
+        // return hex_to_float(value)
+        return 123
     }
 }
